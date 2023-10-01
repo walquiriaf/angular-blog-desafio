1d3a45c84362ea68af03924254036e4925c71962
Walquíria Ferreira <walquiriaferreira03@gmail.com>
2023-10-01 16:26:03 -0300

    blog angular DIO projeto

---
+++ angular-blog/src/app/pages/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVA MÍSTICA ANUNCIADA",
+        "description": "Marvel Studios anuncia o início do multiverso para os X-men e anuncia nova atriz para o papel de Mística",
+        "photoCover": "https://s.aficionados.com.br/imagens/mistica1.jpg"
+    },
+
+    {
+        "id": "2",
+        "title": "As melhores HQ's dos X-men",
+        "description": "lorem ipsumlorem ipsumlorem ipsum",
+        "photoCover": "https://ovicio.com.br/wp-content/uploads/2023/03/20230329-ovicio-x-men-no-mcu.jpg"
+    },
+]
